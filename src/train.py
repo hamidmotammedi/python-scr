db9a82c5a56993f889c46c9c98fb42040b7201b0
unknown <hmotammedi@gmail.com>
2022-03-03 04:17:24 +0330

    DIG-300 add model training

---
+++ src/train.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy import imag
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+
+# Read csv-file
+data = pd.read_csv('data/auto-mpg-training-data.csv', sep=";")
+
+# print(data)
+
+# Shuffle data
+data = data.sample(frac=1)
+
+# 'class'-columns
+y_variable = data['mpg']
+
+# all columns that are not the 'class'-column -> all columns that
+x_variables = data.loc[:, data.columns != 'mpg']
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x_variables, y_variable, test_size=0.2)
+
+regressor = LinearRegression()
+regressor = regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
+
+print(y_pred)
+
+file_to_write = open("data/models/baummethode_lr.pickle", "wb")
+pickle.dump(regressor, file_to_write)
